91d6e31cd2877c846ea077b24cc71f2f2b9cd891
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-09-09 21:53:44 +0700

    add scrollview login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {Button, Gap, Header, Input, Link} from '../../components';
 
 interface LoginProps {
@@ -9,26 +9,28 @@ const Login: React.FC<LoginProps> = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Header onPress={() => navigation.goBack()} title="Login" />
-      <Gap height={30} />
-      <View style={styles.containerContent}>
-        <Input label="Email" placeholder="Please input your email" />
-        <Input
-          secureTextEntry
-          label="Password"
-          placeholder="Please input your password"
-        />
-        <Link
-          onPress={() => navigation.navigate('ForgotPassword')}
-          title="Forgot password ?"
-        />
-        <Gap height={20} />
-        <Button title="Login" />
-        <Text style={styles.or}>or</Text>
-        <Button
-          onPress={() => navigation.navigate('Register')}
-          title="Create new account"
-        />
-      </View>
+      <ScrollView>
+        <Gap height={30} />
+        <View style={styles.containerContent}>
+          <Input label="Email" placeholder="Please input your email" />
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="Please input your password"
+          />
+          <Link
+            onPress={() => navigation.navigate('ForgotPassword')}
+            title="Forgot password ?"
+          />
+          <Gap height={20} />
+          <Button title="Login" />
+          <Text style={styles.or}>or</Text>
+          <Button
+            onPress={() => navigation.navigate('Register')}
+            title="Create new account"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -43,8 +45,8 @@ const styles = StyleSheet.create({
   containerContent: {
     paddingHorizontal: 20,
     flex: 1,
-    // justifyContent: 'center',
     backgroundColor: 'white',
+    marginBottom: 30,
   },
   or: {
     textAlign: 'center',
